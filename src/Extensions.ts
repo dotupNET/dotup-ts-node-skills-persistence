7692da1260afe0b4e4eb6368dd52d09d5ff06dca
Peter Ullrich <dotup.software@gmail.com>
2019-03-28 04:48:42 +0100

    gulp

--- src/Extensions.ts
+++ src/Extensions.ts
@@ -1,10 +1,10 @@
 // tslint:disable: interface-name
 // tslint:disable: no-submodule-imports
 import { PersistenceAdapter } from 'ask-sdk-core';
-import { IRequestAttributes } from 'dotup-ts-node-skills/dist/Attributes/IRequestAttributes';
+import { IRequestAttributes } from 'dotup-ts-node-skills/dist/Interfaces/IRequestAttributes';
 import { NodeSkill } from 'dotup-ts-node-skills/dist/NodeSkill';
 
-declare module 'dotup-ts-node-skills/dist/Attributes/IRequestAttributes' {
+declare module 'dotup-ts-node-skills/dist/Interfaces/IRequestAttributes' {
   export interface IRequestAttributes {
     savePersistentAttributes: boolean;
     persistentAttributes: {
